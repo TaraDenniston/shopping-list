e8a30e1ef624f1a8720d8a31dbe593d158988249
Tara Denniston <taradenniston@gmail.com>
2023-10-11 08:19:23 -0400

    add PATCH and DELETE routes for /items/:name

--- item.js
+++ item.js
@@ -17,9 +17,26 @@ class Item {
     if (item === undefined) {
       throw {message: 'Item not found', status: 404}
     }
-    return foundItem;
+    return item;
+  }
+
+  static update(name, data) {
+    const item = Item.find(name);
+    if (item === undefined) {
+      throw {message: 'Item not found', status: 404}
+    }
+    item.name = data.name;
+    item.price = data.price;
+    return item;
+  }
+
+  static delete(name) {
+    const index = items.findIndex(ele => ele.name === name);
+    if (index === -1) {
+      throw {message: 'Item not found', status: 404}
+    }
+    items.splice(index, 1);
   }
 }
-
 
 module.exports = Item;
--- routes/items.js
+++ routes/items.js
@@ -3,18 +3,57 @@ const router = new express.Router();
 const ExpressError = require('../expressError');
 const Item = require('../item');
 
+/*******************************************************
+ * Item Routes
+ *******************************************************/
+
+/*** GET / *** Return all items ************************/
 router.get('/', (req, res, next) => {
-  return res.json({ items: Item.findAll() });
+  try {
+    return res.json({ items: Item.findAll() });
+  } catch(err) {
+    return next(err);
+  }
 });
 
+/*** POST / *** Add new item with name & price ********/
 router.post('/', (req, res, next) => {
-  let newItem = new Item(req.body.name, req.body.price);
-  return res.json({ added: newItem });
+  try {
+    let newItem = new Item(req.body.name, req.body.price);
+    return res.json({ added: newItem });
+  } catch (err) {
+    return next(err);
+  }
 });
 
+/*** GET /:name *** Find item that matches name *******/
 router.get('/:name', (req, res, next) => {
-  req.params.name
+  try {
+    let foundItem = Item.find(req.params.name);
+    return res.json({ item: foundItem });
+  } catch(err) {
+    return next(err);
+  }
 });
 
+/*** PATCH /:name *** Update item that matches name ***/
+router.patch('/:name', (req, res, next) => {
+  try {
+    let item = Item.update(req.params.name, req.body);
+    return res.json({ updated: item });
+  } catch(err) {
+    return next(err);
+  }
+});
+
+/*** DELETE /:name *** Delete item that matches name ***/
+router.delete('/:name', (req, res, next) => {
+  try {
+    let item = Item.delete(req.params.name);
+    return res.json('Deleted');
+  } catch(err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
