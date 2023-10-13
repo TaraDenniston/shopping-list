8320688f20a100bb704253fc27fb53cc5db98334
Tara Denniston <taradenniston@gmail.com>
2023-10-13 08:41:02 -0400

    add broad error handling for routes

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ const itemsRoutes = require('./routes/items');
 
 const app = express();
 
+// Format every request into JSON
 app.use(express.json());
 
+// Import all items routes
 app.use('/items', itemsRoutes);
 
+// Display error for all requests that don't have a matching route
+app.use((req, res, next) => {
+  return next(new ExpressError('Page Not Found', 404));
+});
 
+// Display a generic error for all other errors
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  return res.status(status).json({
+    error {
+      message: err.message,
+      status: status
+    }
+  });
+});
 
 module.exports = app;
 
