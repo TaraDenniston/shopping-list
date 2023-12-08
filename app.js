ea4a92644e7589efd572547488dbb5dc3717d463
Tara Denniston <taradenniston@gmail.com>
2023-12-07 19:43:48 -0500

    debugging

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   let status = err.status || 500;
   return res.status(status).json({
-    error {
+    error: {
       message: err.message,
       status: status
     }
